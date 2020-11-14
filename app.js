50f114a2a7712a0088bb06cdd7ac99cbc2263157
Roger Cheng <roger801206@gmail.com>
2020-11-14 14:41:23 +0800

    Generate rubbish depend on job

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const rubbishGenerator = require('./rubbish_content_generator')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const output = rubbishGenerator(req.body)
+  res.render('index', { output })
 })
 
 app.listen(port, () => {
--- rubbish_content_generator.js
+++ rubbish_content_generator.js
@@ -1,19 +1,46 @@
-// task, phrase database
-const task = {
-  engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
-  designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-  entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+function random(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
 }
 
-const phrase = ['很簡單', '很容易', '很快', '很正常']
+function rubbishGenerator(option) {
 
-// dummy data
+  const task = {
+    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
+    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
+  }
 
-// generate rubbish
-  // random task depend on target
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
 
-  // random phrase
+  const output = {
+    rubbish: '',
+    engineer: '',
+    designer: '',
+    entrepreneur: ''
+  }
 
-  // combine all as a rubbish
+  if (!option.job) {
+    output.rubbish += `You must choose a job.`
+    console.log(output)
+  } else {
+    switch (option.job) {
+      case 'engineer':
+        output.engineer += 'checked'
+        output.rubbish += `身為一個工程師，${random(task.engineer)}，${random(phrase)}吧！`
+        break
+      case 'designer':
+        output.designer += 'checked'
+        output.rubbish += `身為一個設計師，${random(task.designer)}，${random(phrase)}吧！`
+        break
+      case 'entrepreneur':
+        output.entrepreneur += 'checked'
+        output.rubbish += `身為一個創業家，${random(task.entrepreneur)}，${random(phrase)}吧！`
+        break
+    }
+    console.log(output)
+  }
+  return output
+}
 
-// output rubbish
+module.exports = rubbishGenerator
